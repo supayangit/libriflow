6973e7af5ad4c19d5f9dac1d066bdd2725f51c64
supayangit <supayanchk@gmail.com>
2026-06-05 20:32:17 +0600

    build fixed

--- src/app/(tabs)/books/page.jsx
+++ src/app/(tabs)/books/page.jsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import ClientBooksPage from "./ClientBooksPage";
 
 export const metadata = {
@@ -5,5 +6,9 @@ export const metadata = {
 };
 
 export default function Page() {
-  return <ClientBooksPage />;
+  return (
+    <Suspense fallback="Loading...">
+      <ClientBooksPage />
+    </Suspense>
+  );
 }
